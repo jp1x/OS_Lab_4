c67ff236ec1b116152742612badc28a3330b5dac
jp1x <vityok_maltsev@mail.ru>
2022-11-27 18:54:19 +0300

    Добавьте файлы проекта.

---
+++ OS_Lab_4/OS_Lab_4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace chrono;
+
+int main()
+{
+    auto start = system_clock::now();
+    int a = 0, b = 3, c = 3;
+
+    for (int i = 0; i < 100000000; i++)
+    {
+        a += 2 * b + c - i;
+    }
+
+    auto end = system_clock::now();
+    auto elapsed = duration_cast<milliseconds>(end - start);
+
+    cout << "Program finished execution within " << (float)elapsed.count() / 1000 << " seconds.\n";
+}
